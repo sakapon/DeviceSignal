91de56d473da4a5c5818f19c7fd09eb9daf86b41
Keiho Sakapon <git@saka-pon.net>
2019-01-10 14:10:02 +0900

    Add classes for hub

---
+++ DeviceSync/DeviceSyncWeb/SensorHub.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+
+namespace DeviceSyncWeb
+{
+    public class SensorHub : Hub
+    {
+        public void UpdateOrientation(double alpha, double beta, double gamma)
+        {
+            Clients.Others.NotifyOrientation(alpha, beta, gamma);
+        }
+    }
+}
